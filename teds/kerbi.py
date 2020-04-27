3db0b84dc7d963a82e351e5db023365758933297
xavier <xavier@codenectar.com>
2020-04-27 18:23:27 +0200

    image with ruby and python

--- teds/kerbi.py
+++ teds/kerbi.py
@@ -5,7 +5,7 @@ from teds import utils as ut
 
 def init():
   if ut.init():
-    command = f"~/.rbenv/shims/bundle"
+    command = f"bundle"
     result = ut.exec_cmd(command, cwd=ut.working_dir())
     print(result)
 
@@ -15,7 +15,7 @@ def interpolate():
   print(paths)
   real_paths = [p for p in paths if p]
   f_paths = " -f ".join(real_paths)
-  command = f"~/.rbenv/shims/ruby app.rb -f {f_paths}"
+  command = f"ruby app.rb -f {f_paths}"
   print(command)
   print(ut.exec_cmd("ls", cwd=ut.working_dir()))
   return ut.exec_cmd(command, cwd=ut.working_dir())
