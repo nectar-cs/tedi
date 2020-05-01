4d166429a9c5bb4ef9de6da042d61fc7b1979f69
xavier <xavier@codenectar.com>
2020-05-01 23:11:04 +0200

    linear defaults dirs

--- teds/utils.py
+++ teds/utils.py
@@ -3,8 +3,8 @@ import subprocess
 from pathlib import Path
 
 
-clone_into_dir = lambda: os.environ.get('CLONE_INTO_DIR', '/tmp/teds/in')
-working_dir = lambda: os.environ.get('WORKING_DIR', '/tmp/teds/work')
+clone_into_dir = lambda: os.environ.get('CLONE_INTO_DIR', '/tmp/teds-in')
+working_dir = lambda: os.environ.get('WORKING_DIR', '/tmp/teds-work')
 in_repo_subpath = lambda: os.environ.get('REPO_SUBPATH', '')
 repo_name = lambda: os.environ.get('REPO_NAME')
 overrides_path = lambda: os.environ.get('OVERRIDES_PATH')
